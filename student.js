43561559bd3f8ed869f749f619fb9f7fc4a6eae7
MPeezy <marjoriepatton@rocketmortgage.com>
2022-01-11 08:18:52 -0500

    initial

---
+++ student.js
@@ -0,0 +1,74 @@
+function student()
+{
+    let password = prompt("Please input a student name");
+    console.log(); 
+
+//if name is equal to student name then print out their attributes
+//cant figure out arrays
+
+
+let name = name;
+let hometown = hometown;
+let favfood=favfood;
+
+
+
+string[] name = new string[11];
+
+            name[0] = " ";
+            name[1] = "Andy Beer";
+            name[2] = "Cassly Sullen";
+            name[3] = "Phillip Conrad";
+            name[4] = "Zachary Parr";
+            name[5] = "Cortez Christian";
+            name[6] = "Erin Walter";
+            name[7] = "Richard Moss";
+            name[8] = "James Mitchell";
+            name[9] = "Rick Magdaleno";
+            name[10] = "Calyn Greene";
+
+            string[] hometown = new string[11];
+            homeTown[0] = " ";
+            homeTown[1] = "Berkley, MI";
+            homeTown[2] = "Detroit, MI";
+            homeTown[3] = "Canton, MI";
+            homeTown[4] = "Wyandotte, MI";
+            homeTown[5] = "Detroit, MI";
+            homeTown[6] = "Troy, MI";
+            homeTown[7] = "Canton, MI";
+            homeTown[8] = "Yap, FSM";
+            homeTown[9] = "Gilbert, AZ";
+            homeTown[10] = "Portage, MI";
+
+            string[] favfood = new string[11];
+            favoriteFood[0] = " ";
+            favoriteFood[1] = "French Fries";
+            favoriteFood[2] = "Steak";
+            favoriteFood[3] = "Fried Chicken";
+            favoriteFood[4] = "Sushi";
+            favoriteFood[5] = "Chicken Fettuccine Alfredo";
+            favoriteFood[6] = "Tacos";
+            favoriteFood[7] = "Sushi";
+            favoriteFood[8] = "Katsu";
+            favoriteFood[9] = "Hamburger";
+            favoriteFood[10] = "Mac and Cheese";
+
+
+
+            
+
+           console.log ("Welcome to our C# class. ");
+           let answer=prompt("Which student would you like to learn about? (Enter a number between 1-10");
+               
+
+                if (answer >= 1 && answer <= 10)
+                {
+                   
+
+
+                       
+
+       
+            }
+
+}
